891444befcff84368807a4527871b0c8a7470684
nwmn <jknewman@ucdavis.edu>
2017-07-27 19:14:06 -0700

    update statsCounter

---
+++ assets/js/stats.js
@@ -0,0 +1,7 @@
+var sc_project=11407531;
+var sc_invisible=1;
+var sc_security="1dfee703";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+"statcounter.com/counter/counter.js'></"+"script>");
